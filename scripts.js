306a204b0228d272e2e2e91478c8a29c7b1e4d6c
Christian Wallace <christianwallace@christians-mbp.lan>
2020-10-02 17:10:37 -0500

    Add demo code for button click functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 const button = document.querySelector("button");
+const p = document.querySelector("p");
 
+p.textContent = "Hello World";
 //  TODO: listen for a 'click' on the 'button'
+button.addEventListener("click", function () {
+  console.log("click");
+});
+
 // TODO: when there is a click change the 'p' tag to say...
